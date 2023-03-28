1432fd851c90e19fd8c73e43774c60ca4ba2ee23
Dmytro Zhelieznyi <qozzez1337@gmail.com>
2023-03-28 10:31:28 +0200

    README.md was created

---
+++ README.md
@@ -0,0 +1,8 @@
+Before you start you have to install java 17+, maven 3.8.6+
+
+To start the project you have to run next commands:
+
+1) mvn clean install
+2) java -jar .\target\nasa-asteroids-fullstack-back-0.0.1-SNAPSHOT.jar
+
+Application will be run on localhost:8080
--- src/test/java/com/example/nasaasteroidsfullstack/NasaAsteroidsFullstackApplicationTests.java
+++ src/test/java/com/example/nasaasteroidsfullstack/NasaAsteroidsFullstackApplicationTests.java
@@ -1,9 +1,18 @@
 package com.example.nasaasteroidsfullstack;
 
+import com.example.nasaasteroidsfullstackback.config.ExecutorServiceConfig;
+import com.example.nasaasteroidsfullstackback.config.PropertyConfig;
+import com.example.nasaasteroidsfullstackback.config.RestTemplateConfig;
+import com.example.nasaasteroidsfullstackback.config.WebClientConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = {
+		ExecutorServiceConfig.class,
+		PropertyConfig.class,
+		RestTemplateConfig.class,
+		WebClientConfig.class
+})
 class NasaAsteroidsFullstackApplicationTests {
 
 	@Test
